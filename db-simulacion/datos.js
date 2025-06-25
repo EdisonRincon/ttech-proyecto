e446ff0d16a918931798118f72da3d3915398e24
EdisonRincon <edisonrinconc@gmail.com>
2025-06-25 16:19:01 -0500

    Mostrar productos, detalleproductos y el contador del carrito

--- db-simulacion/datos.js
+++ db-simulacion/datos.js
@@ -13,3 +13,39 @@ categorias = {
 };
 
 localStorage.setItem("categorias", JSON.stringify(categorias));
+
+// Inicialización de banners en localStorage
+banners = {
+    "101" : { codigo: "101", titulo: "Arroz, Frijoles, Lentejas y Más.", subtítulo:"Calidad y economía para tu mesa diaria.",
+                descripcion:"Explora nuestra selección de granos secos esenciales para tu alimentación balanceada.", imagen: "img/Granos básicos.jpeg" },
+    "102" : { codigo: "102", titulo: "Banano, Tomate, Cebolla y más.", subtítulo:"Frescura y sabor directo del campo.",
+                descripcion:"Compra productos frescos como manzanas, zanahorias, lechugas y más, ideales para tu salud.", imagen: "img/Frutas y verduras.jpeg" },   
+    "103" : { codigo: "103", titulo: "Jugos, Cervezas, Aguardiente.", subtítulo:"Para compartir, refrescar y celebrar.",
+                descripcion:"Gran variedad en refrescos, vinos, energizantes y bebidas tradicionales.", imagen: "img/Bebidas y licores.jpeg" },
+    "104" : { codigo: "104", titulo: "Jabones, Cloro, Papel Higiénico.", subtítulo:"Cuidado personal y del hogar siempre a tu alcance.",
+                descripcion:"Encuentra productos esenciales para mantener tu casa limpia y tú bien cuidado.", imagen: "img/Aseo Personal y Limpieza.jpeg" },
+    "105" : { codigo: "105", titulo: "Pan aliñado, Pandebonos, Tortas.", subtítulo:"Recién horneados, con sabor casero.",
+                descripcion:"Disfruta de productos tradicionales y dulces perfectos para cada momento del día.", imagen: "img/Panadería y Pastelería.jpeg" },
+    "106" : { codigo: "106", titulo: "Leche, Queso, Yogur y más.", subtítulo:"Nutrición y frescura en cada bocado.",
+                descripcion:"Productos lácteos frescos y huevos de calidad para tu alimentación diaria.", imagen: "img/Lácteos y Huevos.jpeg" }, 
+    "107" : { codigo: "107", titulo: "Pollo, Res, Pescado Fresco", subtítulo:"Proteínas frescas y variadas para tu mesa cada día.",
+                descripcion:"Lleva cortes frescos de carne, pollo, pescado y embutidos para tus comidas favoritas.", imagen: "img/Carnes y pescados.jpeg" }
+};
+localStorage.setItem("banners", JSON.stringify(banners));
+
+// Inicialización de productos en localStorage
+Productos = {
+    "1011" : { codigo: "1011", nombre: "Arroz", descripcion: "Arroz por libras", categoria: "101", imagen: "img/Arroz.jpeg", precio:  1950, duración: " 4 semanas", 
+        clasificacion: 4, contenido: ["Seleccionado, fuente de fibra, no contiene grasas, libre de gluten."]},
+    "1021" : { codigo: "1021", nombre: "Piña", descripcion: "Piña por unidad", categoria: "102", imagen: "img/Pinia.jpeg", precio: 3470, duración: " 4 semanas", 
+        clasificacion: 4, contenido: ["Variedad Oro Miel, dulce y jugosa, rica en vitamina C, maduración natural"]},
+    "1031" : { codigo: "1031", nombre: "Whisky Johnnie Walker", descripcion: "Whisky por unidad", categoria: "103", imagen: "img/Whisky Johnnie Walker.jpeg", precio: 104400, duración: " 4 semanas", 
+        clasificacion: 4, contenido: ["Sello Rojo, sabor suave y equilibrado, ideal para celebraciones, botella de 750 ml"]},
+    "1051" : { codigo: "1051", nombre: "Pan de leche", descripcion: "Paquete de 4 unidades", categoria: "105", imagen:"img/Pan de leche.jpeg", precio: 2500, duración: " 4 semanas", 
+        clasificacion: 4, contenido: ["Fresco, suave, esponjoso y con un toque dulce"]},
+    "1061" : { codigo: "1061", nombre: "Huevos", descripcion: "Panal de 30 huevos tipo AAA", categoria: "106", imagen: "img/Huevos AAA en panal.jpeg", precio: 18000, duración: " 4 semanas", 
+        clasificacion: 4, contenido: ["Huevos frescos, de gallinas alimentadas con maíz, ricos en proteínas y vitaminas"]},
+    "1071" : { codigo: "1071", nombre: "Carne fina", descripcion: "Carne empacada por peso", categoria: "107", imagen: "img/Carne fina-libra.jpeg", precio: 18000, duración: " 4 semanas", 
+        clasificacion: 4, contenido: ["Carne de alta calidad, seleccionada y procesada para garantizar frescura y sabor"]}            
+};
+localStorage.setItem("Productos", JSON.stringify(Productos));
--- db-simulacion/funciones.js
+++ db-simulacion/funciones.js
@@ -10,8 +10,133 @@ function obtenerMap(nombre) {
         return map;
 }
 
-// Devolver la lista de categorias almacenadas 
+// Devolver la lista de categorias almacenadas en el localStorage 
 function  obtenerCategorias() {
     const map = obtenerMap("categorias");
+    return map.values();   
+}
+
+// Devolver la lista de banners almacenados en el localStorage
+function  obtenerBanners() {
+    const map = obtenerMap("banners");
     return map.values();
+}
+
+//Devolver la lista de productos almacenados en el localStorage
+function obtenerProductos(categoria = "todos"){
+    const map = obtenerMap("Productos");
+    //return map.values();
+    let productos = [];
+    if(categoria == null || categoria === "todos"){
+        productos = map.values();       
+    } else {
+        productos = map.values().filter(prod => prod.categoria === categoria);
+    }
+    return productos;
+}
+
+// Funciones para la manipulación del carrito de compras
+// Obtener el carrito de compras almacenado en sessionStorage
+function obtenerCarrito() {
+    const str = sessionStorage.getItem('carrito');
+    const carrito = str ? JSON.parse(str) : [];
+    return carrito;
+}
+// Mostrar los productos añadidos al carrito de compras
+function mostrarCarrito() {
+        const carrito = obtenerCarrito();
+        const carritoContainer = document.getElementById('carrito');
+        carritoContainer.innerHTML = "";
+        let precio = 0;
+
+        carrito.forEach((producto,posicion) => {
+            const li = document.createElement('li');
+            li.className = 'list-group-item d-flex justify-content-between mb-2';
+            li.innerHTML = `
+                <div class="d-flex justify-content-between align-items-center w-100">
+                    <div>
+                        <h6 class="my-0">${producto.nombre} x${producto.cantidad}</h6>
+                        <small>$ ${producto.precio * producto.cantidad}</small>
+                    </div>
+                    <a class="btn btn-danger text-decoration-none text-white" href="#" onclick="eliminarProducto(${posicion})" >
+                        <i class="fas fa-times"></i>
+                    </a>
+                </div>`;
+            carritoContainer.appendChild(li);
+            precio += producto.precio * producto.cantidad; //precio = precio + producto.precio;
+        });
+        const totalElement = document.getElementById('total');
+        if (totalElement) totalElement.textContent = precio.toFixed(2);
+        //document.getElementById("contadorCarrito").textContent = carrito.length;
+        const totalUnidades = carrito.reduce((total, item) => total + item.cantidad, 0);
+        const spanContador = document.getElementById("contadorCarrito");
+        if (spanContador) spanContador.textContent = totalUnidades;   
+}
+
+function agregarProducto(producto, actualizarCarrito = true) {
+    let carrito = obtenerCarrito();
+   // carrito.push(productos);
+    const index = carrito.findIndex(item => item.codigo === producto.codigo);
+    if (index !== -1) {
+        carrito[index].cantidad = (carrito[index].cantidad || 1) + 1;
+    } else {
+        carrito.push({ ...producto, cantidad: 1 });
+    }
+
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    //sessionStorage.setItem("contadorCarrito", carrito.length); 
+    
+    const totalUnidades = carrito.reduce((total, item) => total + item.cantidad, 0);
+    sessionStorage.setItem("contadorCarrito", totalUnidades);
+
+    const spanContador = document.getElementById("contadorCarrito");
+    if (spanContador) spanContador.textContent = totalUnidades;
+
+    if(actualizarCarrito === true){                  
+        mostrarCarrito();
+    }
+}  
+
+// Eliminar un producto del carrito de compras
+function eliminarProducto(posicion) {
+    let carrito = obtenerCarrito();
+    carrito.splice(posicion, 1);
+    // Actualizar el carrito en sessionStorage
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    //sessionStorage.setItem("contadorCarrito", carrito.length);
+    const totalUnidades = carrito.reduce((total, item) => total + item.cantidad, 0);
+    sessionStorage.setItem("contadorCarrito", totalUnidades);
+
+    const spanContador = document.getElementById("contadorCarrito");
+    if (spanContador) spanContador.textContent = totalUnidades;
+    
+    mostrarCarrito();
+}
+
+// Vaciar el carrito de compras
+function vaciarCarrito() {
+   
+    sessionStorage.setItem("carrito", JSON.stringify([]));
+    sessionStorage.setItem("contadorCarrito", 0);
+    
+    const spanContador = document.getElementById("contadorCarrito");
+    if (spanContador) spanContador.textContent = 0;
+    mostrarCarrito();
+}
+//Obtener producto por código
+function obtenerProductoPorCodigo(codigoProducto) {
+    const productos = obtenerMap("Productos");
+    const respuesta = productos.get(codigoProducto);
+    if (!respuesta) {
+        throw new Error(`No se encontró el producto con el código: ${codigoProducto}`);
+    }
+    return respuesta;
+}
+function cargarFragmento(idElemento, ruta, callback = null) {
+    fetch(ruta)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(idElemento).innerHTML = html;
+            if (callback) callback(); // Ejecuta el callback si se da
+        });
 }
